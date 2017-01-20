427928a18fda205bea9681500a6c122f3fab3b2e
cloud <lehaoson@gmail.com>
2017-01-20 13:15:33 +0700

    Move entry to upper level folder (src-root); Only 2 entry is fine to put at root.

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import wings from './src/entry/app';
+import wings from './src/app';
 
 AppRegistry.registerComponent('wings', () => wings);
